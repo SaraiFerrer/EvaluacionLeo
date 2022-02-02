b2c0a0d0d58ca0ebbfc9f26164a80236f8edc155
SaraiFerrer <87380666+SaraiFerrer@users.noreply.github.com>
2022-02-02 12:13:20 -0600

    Add files via upload

---
+++ wrecking-ball-master/wrecking ball/Box.js
@@ -0,0 +1,17 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction':1.0,
+        'density':0.04,
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+   
+  }
+}
